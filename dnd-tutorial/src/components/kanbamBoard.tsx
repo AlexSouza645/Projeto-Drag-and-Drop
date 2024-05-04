b263c77d6dd5e393c6b37a839dae9e091c18d6a5
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2024-05-04 19:12:03 -0300

    criando funções  button

--- dnd-tutorial/src/components/kanbamBoard.tsx
+++ dnd-tutorial/src/components/kanbamBoard.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react"
+import PlusIcon from "../icons/PlusIcon"
+import { Column } from "../types";
 
 
 function KanbamBoard() {
+
+    const [columns, setColumns] = useState<Column[]>([]);
+    console.log(columns)
     return (
         <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40]
     justify-center">
 
             <div className="m-auto">
-                <button className=" h-[60px] w-[350px] min-w-[350px] 
-        cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor
-        p-4 ring-rose-500 hover:ring-2" >
+                <button onClick={() => {
+                    createNewColumn()
+                }}
+                    className=" h-[60px] w-[350px] min-w-[350px] 
+                cursor-pointer rounded-lg bg-mainBackgroundColor border-2
+             border-columnBackgroundColor
+               p-4 ring-rose-500 hover:ring-2 flex gap-2" >
+                    <PlusIcon />
 
 
 
@@ -18,6 +29,20 @@ function KanbamBoard() {
 
         </div>
     )
-}
+    function createNewColumn() {
+        const columnToAdd: Column ={
+            id: generateId(),
+            title:`Column ${columns.length + 1}`
+        };
+        setColumns([...columns, columnToAdd])
 
+    }
+
+    function generateId(){
+
+
+        //  generate a random number between 0 and 100 
+          return  Math.floor(Math.random() * 10001 ) 
+}
+}
 export default KanbamBoard

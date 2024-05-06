efc290d9f3e17aa8c57e8224a3d519da4ba288c5
alex miranda de souza <59890212+AlexSouza645@users.noreply.github.com>
2024-05-06 10:39:35 -0300

    adicionando drag and drop

--- dnd-tutorial/src/components/kanbamBoard.tsx
+++ dnd-tutorial/src/components/kanbamBoard.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import PlusIcon from "../icons/PlusIcon"
-import { Column } from "../types";
+import { Column, Id } from "../types";
 import ColumnContainer from "./ColumnContainer";
+import { DndContext } from "@dnd-kit/core";
 
 
 function KanbamBoard() {
@@ -11,32 +12,35 @@ function KanbamBoard() {
     return (
         <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40]
     justify-center">
+            <DndContext>
+                <div className="m-auto flex gap-4">
+                    <div className=" flex gap-4">
 
-            <div className="m-auto flex gap-4">
-                <div className=" flex gap-4">
+                        {columns.map((col) => (
 
-                    {columns.map((col) => (
+                            <ColumnContainer key={col.id} column={col} deleteColumn={deleteColumn} />
 
-                     <ColumnContainer column={col}/>
-
-                    ))}
-                </div>
-                <button onClick={() => {
-                    createNewColumn()
-                }}
-                    className=" h-[60px] w-[350px] min-w-[350px] 
+                        ))}
+                    </div>
+                    <button onClick={() => {
+                        createNewColumn()
+                    }}
+                        className=" h-[60px] w-[350px] min-w-[350px] 
                 cursor-pointer rounded-lg bg-mainBackgroundColor border-2
              border-columnBackgroundColor
                p-4 ring-rose-500 hover:ring-2 flex gap-2" >
-                    <PlusIcon />
+                        <PlusIcon />
 
 
 
-                    Add Column
-                </button>
-            </div>
-
+                        Add Column
+                    </button>
+                </div>
+            </DndContext >
         </div>
+
+
+
     )
     function createNewColumn() {
         const columnToAdd: Column = {
@@ -47,11 +51,15 @@ function KanbamBoard() {
 
     }
 
-    function generateId() {
-
-
-        //  generate a random number between 0 and 100 
-        return Math.floor(Math.random() * 10001)
+    function deleteColumn(id: Id) {
+        const filteredColumns = columns.filter((col) => col.id !== id);
+        setColumns(filteredColumns)
     }
+}
+function generateId() {
+
+
+    //  generate a random number between 0 and 100 
+    return Math.floor(Math.random() * 10001)
 }
 export default KanbamBoard
